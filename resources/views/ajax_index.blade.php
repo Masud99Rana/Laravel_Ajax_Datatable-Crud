316c8b84f23491bd1e1fc169526ea3de7379304b
Masud99Rana <Masudrana917775@gmail.com>
2019-05-20 23:34:46 +0600

    DataTables Server Side Processing using Ajax is Completed

--- resources/views/ajax_index.blade.php
+++ resources/views/ajax_index.blade.php
@@ -1,7 +1,7 @@
 <html>
  <head>
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Laravel 5.8 - DataTables Server Side Processing using Ajax</title>
+  <title> DataTables Server Side Processing using Ajax </title>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
   <script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
@@ -12,7 +12,7 @@
  <body>
   <div class="container">    
      <br />
-     <h3 align="center"Laravel 5.8 Ajax Crud  - Delete or Remove Data</h3>
+     <h3 align="center"> Ajax Crud  - Delete or Remove Data  </h3>
      <br />
      <div align="right">
       <button type="button" name="create_record" id="create_record" class="btn btn-success btn-sm">Create Record</button>
@@ -133,6 +133,11 @@ $(document).ready(function(){
 
  $('#create_record').click(function(){
   $('.modal-title').text("Add New Record");
+
+
+     $('#sample_form')[0].reset();
+     $('#store_image').html("");
+
      $('#action_button').val("Add");
      $('#action').val("Add");
      $('#formModal').modal('show');
